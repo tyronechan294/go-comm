999df5420f2d1b2f244da481020fd0f2890fb075
tyrone <tyronechan294@gmail.com>
2024-05-17 23:21:56 +0800

    Init

--- servicecontexth/servicecontext.go
+++ servicecontexth/servicecontext.go
@@ -2,6 +2,7 @@ package servicecontexth
 
 import (
 	"github.com/apache/rocketmq-client-go/v2"
+	"github.com/fbsobreira/gotron-sdk/pkg/client"
 	"github.com/redis/go-redis/v9"
 	"gorm.io/gorm"
 )
@@ -12,4 +13,6 @@ type ServiceContext struct {
 
 	RocketProducer     rocketmq.Producer
 	RocketPushConsumer rocketmq.PushConsumer
+
+	TronGrpcClient *client.GrpcClient
 }
