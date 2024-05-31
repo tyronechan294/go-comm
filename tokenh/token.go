3b5f315048ce27aff132f0e4b5a5b0fd9e06505b
tyrone <tyronechan294@gmail.com>
2024-05-31 10:21:58 +0800

    update tokenh

--- tokenh/token.go
+++ tokenh/token.go
@@ -1,12 +1,11 @@
 package tokenh
 
 import (
-	"github.com/gogf/gf/v2/util/gconv"
 	"github.com/gogf/gf/v2/util/grand"
 	"github.com/tyronechan294/go-comm/md5h"
 	"github.com/tyronechan294/go-comm/randh"
 )
 
-func New(id uint64, salt string) string {
-	return md5h.Enc(gconv.String(id)+salt) + grand.Str(randh.AlphaNumeric, 32)
+func New(id string, salt string) string {
+	return md5h.Enc(id+salt) + grand.Str(randh.AlphaNumeric, 32)
 }
